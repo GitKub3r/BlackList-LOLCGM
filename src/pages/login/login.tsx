667132850eadb8793e8aecdba525fead6ddf7d3e
Sergio <sergiogonzalezalcantara@gmail.com>
2024-12-04 16:46:54 +0100

    Changed login component

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,8 +1,6 @@
 import { LoginForm } from "../../components/login-form/loginform";
 import "./login.css";
 
-const sayHi = () => console.log("hi");
-
 export const Login = () => {
   return (
     <div className="main-content">
